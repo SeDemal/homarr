9a8b965ff6b49b2538571da212ed9765657c1f46
Tagaishi <Tagaishi@hotmail.ch>
2024-03-03 20:20:40 +0100

    refactor: normalize fetches

--- src/tools/server/sdk/adGuard/adGuard.ts
+++ src/tools/server/sdk/adGuard/adGuard.ts
@@ -1,7 +1,4 @@
-import axios from 'axios';
-import Consola from 'consola';
 import { z } from 'zod';
-import { trimStringEnding } from '~/tools/shared/strings';
 
 import {
   adGuardApiFilteringStatusSchema,
@@ -10,61 +7,22 @@ import {
 } from './adGuard.schema';
 
 export class AdGuard {
-  private readonly baseHostName: string;
-
   constructor(
-    hostname: string,
+    private readonly hostname: string,
     private readonly username: string,
     private readonly password: string
-  ) {
-    this.baseHostName = trimStringEnding(hostname, ['/#', '/']);
-  }
+  ) {}
 
   async getStats(): Promise<AdGuardStatsType> {
-    const response = await fetch(`${this.baseHostName}/control/stats`, {
-      headers: {
-        Authorization: `Basic ${this.getAuthorizationHeaderValue()}`,
-      },
-    });
-
-    if (response.status !== 200) {
-      throw new Error(`AdGuard stats; Status code does not indicate success: ${response.status}`);
-    }
-
-    const data = await response.json();
-
-    return adGuardApiStatsResponseSchema.parseAsync(data);
+    return await this.getData("stats", adGuardApiStatsResponseSchema) as AdGuardStatsType;
   }
 
   async getStatus() {
-    const response = await fetch(`${this.baseHostName}/control/status`, {
-      headers: {
-        Authorization: `Basic ${this.getAuthorizationHeaderValue()}`,
-      },
-    });
-
-    if (response.status !== 200) {
-      throw new Error(`AdGuard status; Status code does not indicate success: ${response.status}`);
-    }
-
-    const data = await response.json();
-
-    return adGuardApiStatusResponseSchema.parseAsync(data);
+    return await this.getData("status", adGuardApiStatusResponseSchema) as AdGuardStatusType;
   }
 
   async getCountFilteringDomains() {
-    const response = await fetch(`${this.baseHostName}/control/filtering/status`, {
-      headers: {
-        Authorization: `Basic ${this.getAuthorizationHeaderValue()}`,
-      },
-    });
-
-    if (response.status !== 200) {
-      throw new Error(`AdGuard domain filters; Status code does not indicate success: ${response.status}`);
-    }
-
-    const data = await response.json();
-    const schemaData = await adGuardApiFilteringStatusSchema.parseAsync(data);
+    const schemaData = await this.getData("filtering/status", adGuardApiFilteringStatusSchema) as AdGuardDomainFiltersType;
 
     return schemaData.filters
       .filter((filter) => filter.enabled)
@@ -78,6 +36,23 @@ export class AdGuard {
     await this.changeProtectionStatus(true);
   }
 
+  private async getData(type: string, schema: z.ZodObject<{}>) {
+    const response = await fetch(new URL(`/control/${type}`, this.hostname), {
+      method: 'GET',
+      headers: {
+        Authorization: `Basic ${this.getAuthorizationHeaderValue()}`,
+      },
+    });
+
+    if (response.status !== 200) {
+      throw new Error(`AdGuard ${type}; Status code does not indicate success: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    return schema.parseAsync(data);
+  }
+
   /**
    * Make a post request to the AdGuard API to change the protection status based on the value of newStatus
    * @param {boolean} newStatus - The new status of the protection
@@ -85,25 +60,22 @@ export class AdGuard {
    * @returns {string} - The response from the AdGuard API
    */
   private async changeProtectionStatus(newStatus: boolean, duration = 0) {
-    try {
-      const { data }: { data: string } = await axios.post(
-        `${this.baseHostName}/control/protection`,
-        {
-          enabled: newStatus,
-          duration,
-        },
-        {
-          headers: {
-            Authorization: `Basic ${this.getAuthorizationHeaderValue()}`,
-          },
-        }
-      );
-      return data;
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        Consola.error(error.message);
-      }
+    const response = await fetch(new URL(`/control/protection`, this.hostname), {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${this.getAuthorizationHeaderValue()}`,
+      },
+      body: JSON.stringify({
+        enabled: newStatus,
+        duration,
+      }),
+    });
+
+    if (response.status !== 200) {
+      throw new Error(`AdGuard Protection ${newStatus}; Status code does not indicate success: ${response.status}`);
     }
+
+    return await response.json();
   }
 
   /**
@@ -116,3 +88,5 @@ export class AdGuard {
 }
 
 export type AdGuardStatsType = z.infer<typeof adGuardApiStatsResponseSchema>;
+export type AdGuardStatusType = z.infer<typeof adGuardApiStatusResponseSchema>;
+export type AdGuardDomainFiltersType = z.infer<typeof adGuardApiFilteringStatusSchema>;
